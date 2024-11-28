eff946aea18582e59dee98eea50b5757865bae65
abhay_dharmale <140411417+abhay-dharmale@users.noreply.github.com>
2024-11-28 10:46:13 +0000

    implement a contact functionality with EmailJs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Home from "./components/Home";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Loading from "./components/Loading";
 import Trending from "./components/Trending";
 import Popular from "./components/Popular";
 import Movies from "./components/Movies";
